1276dcbe7531e7b06d44a46928fc1fdf7d513eac
ana11090 <72621705+ana11090@users.noreply.github.com>
2024-01-14 18:53:27 +0200

    Add project files.

---
+++ Niculae_Ana-Maria_Proiect2/Models/Proiect.cs
@@ -0,0 +1,37 @@
+﻿namespace Niculae_Ana_Maria_Proiect2.Models
+{
+    public class Proiect
+    {
+        public int ProiectId { get; set; }
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Introduceti numele proiectului")]
+        public string Nume { get; set; }
+        public string? Descriere { get; set; }
+        public DateTime? DataIncepere { get; set; }
+        public DateTime? DataFinalizare { get; set; }
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Alegeti statusul proiectului")]
+        public StatusProiect Status { get; set; }
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Alegeti managerul proiectului")]
+        public int ManagerId { get; set; }
+        public Manager? ManagerProiect { get; set; }
+        public List<Sarcina> Sarcini { get; set; }
+        public List<Comentariu> Comentarii { get; set; } // Colecția de comentarii asociate proiectului
+
+        public Proiect()
+        {
+            Sarcini = new List<Sarcina>();
+            Comentarii = new List<Comentariu>();
+            Status = StatusProiect.InAsteptare; // Un status inițial, de exemplu "În Așteptare"
+        }
+    }
+
+    public enum StatusProiect
+    {
+        InAsteptare,
+        InDesfasurare,
+        Completat,
+        Suspendat
+    }
+
+
+}
+}
